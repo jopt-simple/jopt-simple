317bdd2b74059b01dcc83eb8a375a8dc06e549a2
El Kodus <elkodus@gmail.com>
2012-09-29 22:11:46 +0200

    conformity with project formatting style

--- src/main/java/joptsimple/Messages.java
+++ src/main/java/joptsimple/Messages.java
@@ -6,15 +6,15 @@ import java.util.ResourceBundle;
 public class Messages {
     private static final String BUNDLE_NAME = "joptsimple.messages";
 
-    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle( BUNDLE_NAME );
 
     private Messages() {
     }
 
-    public static String getString(String key) {
+    public static String getString( String key ) {
         try {
-            return RESOURCE_BUNDLE.getString(key);
-        } catch (MissingResourceException e) {
+            return RESOURCE_BUNDLE.getString( key );
+        } catch ( MissingResourceException e ) {
             return '!' + key + '!';
         }
     }
